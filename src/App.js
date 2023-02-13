3dba6eb0dbc854c4eff0fd613efe12d37d4657ea
SmOke11345 <103870835+SmOke11345@users.noreply.github.com>
2023-02-13 19:07:58 +0600

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import './index.scss';
+import React, { useEffect, useState } from 'react';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
+import './index.scss';
 
 // Тут список пользователей: https://reqres.in/api/users
 
